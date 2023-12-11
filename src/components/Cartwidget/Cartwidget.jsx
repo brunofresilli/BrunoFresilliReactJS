d476b90e6e30a9609f76a839c494d6b320a8c70c
BrunoFresilli <BrunoFresilli11@gmail.com>
2023-12-10 22:14:24 -0300

    PreEntrega1Fresilli

---
+++ src/components/Cartwidget/Cartwidget.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Cart from '../../assets/cart.png';
+const Cartwidget = () => {
+  return (
+    <div> 
+        <img src={Cart} alt="Cart" style={{ height: '55px', marginRight: '30px',}} />
+        
+    </div>
+  )
+}
+
+export default Cartwidget
---
+++ src/components/Itemlistcontainer/Itemlistcontainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Itemlistcontainer.css";
+
+const Itemlistcontainer = (greeting) => {
+  return (
+    <div className='Itemlistcontainer'>
+        <h3>Bienvenidos a SNEARKERS SHOP!!!</h3>
+    </div>
+  )
+}
+
+export default Itemlistcontainer
